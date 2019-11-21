96963e0c71b80799f4f7ca71298ce2fbc1f16a9a
Maxim Golman <sankeman2004@gmail.com>
2019-11-21 13:55:11 +0200

    refactor schema creation for better results. now values inside arrays don't lose information between different results

--- src/schemaCreator.ts
+++ src/schemaCreator.ts
@@ -3,104 +3,57 @@ import { resolveValueArchtype, resolveType } from './utils/generalUtil';
 
 function mapObjectToSchema(obj: SomeObj, schema: SomeObj = {}): SomeObj {
     return Object
-        .keys(obj)
+        .entries(obj)
         .reduce(
-            (schema, key) => resolveOperation(schema, obj, key),
+            (accSchema, [key, value]) => Object.assign(accSchema, { [key]: resolveOperation(schema[key], value) }),
             schema
         );
 }
 
-function handleEmptyValue(schema: SomeObj, key: string) {
-    const value = schema[key];
-    if (value == null || value == 'unknown') {
-        return unknownType(schema, key);
-    }
-    return schema;
+function resolveOperation(schema: SomeObj = {}, obj: any): SomeObj {
+    const schemaArchType = resolveValueArchtype(schema);
+    const objArchType = resolveValueArchtype(obj);
+
+    const strategy = resolveStrategy(objArchType, schemaArchType);
+    return strategy(obj, schema);
 }
 
 function resolveStrategy(obj: ValArchtype, schema: ValArchtype): Function {
-    if (
-        obj == 'SIMPLE' && schema == 'EMPTY'
-    ) {
+    if (obj == 'SIMPLE') {
         return substitution;
-    } else if (
-        obj == 'EMPTY'
-        || obj == 'SIMPLE' && schema != 'SIMPLE'
-    ) {
-        return discard;
-    } else if (
-        obj == 'ARRAY' && (schema == 'EMPTY' || schema == 'ARRAY')
-    ) {
+
+    } else if (obj == 'ARRAY') {
         return arrayRecursion;
-    } else if (
-        obj == 'OBJECT' && (schema == 'EMPTY' || schema == 'OBJECT')
-    ) {
-        return objectRecursion;
+
+    } else if (obj == 'EMPTY' && schema == 'EMPTY') {
+        return unknownType;
+
+    } else if (obj == 'EMPTY') {
+        return discard;
+
+    } else if (obj == 'OBJECT') {
+        return mapObjectToSchema;
+
+    } else {
+        return unknownType;
     }
 }
 
-function discard(obj: SomeObj, schema: SomeObj, key: string) {
+function arrayRecursion(obj: any[], schema: SomeObj = {}): any[] {
+    const arraySchema = obj.reduce((accSchema, subObj) => resolveOperation(Array.isArray(accSchema) ? accSchema[0] : accSchema, subObj), schema);
+    return Array.isArray(arraySchema) ? arraySchema : [arraySchema];
+}
+
+function discard(obj: SomeObj, schema: SomeObj) {
     return schema;
 }
 
-function substitution(obj: SomeObj, schema: SomeObj, key: string) {
-    return simpleType(resolveType(obj[key]), key, schema);
+function substitution(obj: SomeObj, schema: SomeObj) {
+    return JSON.stringify({ type: resolveType(obj).toLowerCase() });
 }
 
-function arrayRecursion(obj: any[], schema: SomeObj, key: string) {
-    const arraySchema = obj[key].reduce((accSchema, subObj) => resolveOperation(accSchema, subObj, key), {});
-    return Object.assign(schema, arraySchema);
-}
-
-function objectRecursion(obj: SomeObj, schema: SomeObj, key: string) {
-    const subObjectSchema = Object
-        .entries(obj[key])
-        // .reduce((accSchema, subObj) => ; // TODO: finish
-    return Object.assign(schema)
-
-}
-
-function resolveOperation(schema: SomeObj, obj: any, key: string): SomeObj {
-    const objNestedValue = obj[key];
-    const schemaNestedValue = schema[key];
-    const schemaArchType = resolveValueArchtype(schema[key]);
-    const objArchType = resolveValueArchtype(objNestedValue);
-
-    const strategy = resolveStrategy(objArchType, schemaArchType)
-
-    // todo: all below is not correct
-    switch (objArchType) {
-        case 'EMPTY':
-            return handleEmptyValue(schema, key);
-        case 'SIMPLE':
-            const type = resolveType(objNestedValue);
-            return simpleType(type, key, schema);
-        case 'ARRAY':
-            return objNestedValue.length > 0
-                ? Object.assign(schema, { [key]: [arrayOfObjectsToSchema(objNestedValue, {})] })
-                : schema;
-        case 'OBJECT':
-            return Object.keys(objNestedValue).length > 0
-                ? Object.assign(schema, { [key]: mapObjectToSchema(objNestedValue, {}) })
-                : schema;
-    }
-}
-
-function arrayOfObjectsToSchema(array: any[], schema: SomeObj = {}): SomeObj {
-    return array.reduce(
-        (acc, obj) => resolveValueArchtype(obj) == 'OBJECT'
-            ? mapObjectToSchema(obj, acc)
-            : JSON.stringify({ type: resolveType(obj).toLowerCase() }),
-        schema
-    );
-}
-
-function simpleType(type: string, key: string, schema: SomeObj) {
-    return Object.assign(schema, { [key]: JSON.stringify({ type: type.toLowerCase() }) });
-}
-
-function unknownType(schema: SomeObj, key: string) {
-    return Object.assign(schema, { [key]: JSON.stringify({ type: 'unknown' }) });
+function unknownType(obj: SomeObj, schema: SomeObj) {
+    return JSON.stringify({ type: 'unknown' });
 }
 
 export { mapObjectToSchema };
